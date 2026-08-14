7540708b3c8ade6eb1fefa393cf6edfcd34901ae
Diễm My <mynhd1575@ut.edu.vn>
2026-08-14 17:43:42 +0700

    Delete Code directory

--- Code/ChatTCP.Server/Form1.cs
+++
@@ -1,9 +0,0 @@
-namespace ChatTCP.Server;
-
-public partial class Form1 : Form
-{
-    public Form1()
-    {
-        InitializeComponent();
-    }
-}
--- Code/ChatTCP.Shared/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace ChatTCP.Shared;
-
-public class Class1
-{
-
-}
