2a7561f8826ea99d622492ccdd129be8c4905a16
lqphu369 <phulq3580@ut.edu.vn>
2026-08-22 18:23:42 +0700

    Thêm thông tin trưởng nhóm và kiểm tra thành viên

--- 012012301303_Group9_UDM08/Code/ChatTCP.Shared/Models/Group.cs
+++ 012012301303_Group9_UDM08/Code/ChatTCP.Shared/Models/Group.cs
@@ -6,11 +6,46 @@ namespace ChatTCP.Shared.Models
     public class Group
     {
         public int GroupId { get; set; }
-        public int Id { get => GroupId; set => GroupId = value; }
-        public string GroupName { get; set; } = string.Empty;
-        public string Name { get => GroupName; set => GroupName = value; }
+
+        public int Id
+        {
+            get => GroupId;
+            set => GroupId = value;
+        }
+
+        public string GroupName { get; set; }
+            = string.Empty;
+
+        public string Name
+        {
+            get => GroupName;
+            set => GroupName = value;
+        }
+
         public int CreatedBy { get; set; }
-        public DateTime CreatedAt { get; set; } = DateTime.Now;
-        public List<int> MemberIds { get; set; } = new List<int>();
+
+        public int OwnerId
+        {
+            get => CreatedBy;
+            set => CreatedBy = value;
+        }
+
+        public DateTime CreatedAt { get; set; }
+            = DateTime.Now;
+
+        public List<int> MemberIds { get; set; }
+            = new List<int>();
+
+        public bool IsOwner(int userId)
+        {
+            return userId > 0 &&
+                   CreatedBy == userId;
+        }
+
+        public bool HasMember(int userId)
+        {
+            return userId > 0 &&
+                   MemberIds.Contains(userId);
+        }
     }
 }
