2608668c3b23ca3f64c5fdb2e17d48d718c6c511
lqphu369 <phulq3580@ut.edu.vn>
2026-08-22 18:24:43 +0700

    Sửa lỗi trùng tên Message trong TcpServer

--- 012012301303_Group9_UDM08/Code/ChatTCP.Server/Network/TcpServer.cs
+++ 012012301303_Group9_UDM08/Code/ChatTCP.Server/Network/TcpServer.cs
@@ -3,7 +3,7 @@ using System.Net;
 using System.Net.Sockets;
 using System.Threading;
 using ChatTCP.Shared.Models;
-
+using Message = ChatTCP.Shared.Models.Message;
 namespace ChatTCP.Server.Network
 {
     public class TcpServer
